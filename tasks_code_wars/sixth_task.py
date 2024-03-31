00ac336c1944b1547b0d15db5f8ae1c29574e82a
Pawelooo <paweloo0147@gmail.com>
2024-03-31 15:42:49 +0200

    New task

---
+++ tasks_code_wars/sixth_task.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/python
+def duplicate_encode(word):
+    new = ''
+    used = []
+    for w in word:
+        if w in used:
+            new += ')'
+        else:
+            num = 0
+            for n in word:
+                if w.lower() == n.lower():
+                    num += 1
+            new += '(' if num == 1 else ')'
+            used.append(w.lower())
+
+    return new
